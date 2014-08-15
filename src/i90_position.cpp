a4912ece73066bb6562b28afa8f77eaf66fbe9f9
shahidare <shahid@shahids-mbp.default>
2014-08-15 23:23:04 +0100

    Updated
    Removed publishing from rotation function and added comments

--- src/i90_position.cpp
+++ src/i90_position.cpp
@@ -15,8 +15,8 @@ After the estimation is done, a signal published on i90_estimation_done*/
 #include "ros/ros.h"
 #include "pos.h"//position values(x,y,yaw)
 #include "MotorInfoArray.h"//encoder readings
-#include <math.h>
-#include <std_msgs/UInt8.h>
+#include <math.h>  // Library for sin and cosine functions
+#include <std_msgs/UInt8.h> // Standard int message type for ros
 
 /*Variables*/
 int iPrevEnc[2]={0,0};//0:left wheel, 1:right wheel Previous encoder readings
@@ -25,9 +25,9 @@ int iEncoderDifference[2]; //Difference of Current encoder reading and previous 
 float fPrevPosX=0;// Previous x position of i90 
 float fPrevPosY=0;// Previous y position of i90 
 float fPrevYaw=45;// previous yaw angle of i90
-int iPubFlag=0;
-i90_position::pos currentPos;
-i90_position::pos prevPos;
+int iPubFlag=0;  // Flag to publish curren position
+i90_position::pos currentPos; // Current i90 position
+i90_position::pos prevPos;    // i90 position for previous state
 
 
 
@@ -48,13 +48,13 @@ int main(int argc, char **argv){
 	ros::Subscriber encoderSub = n.subscribe("drrobot_motor", 1, updateEncoder);            // This function reads the encoder readings and updates current encoder values
 	ros::Duration d = ros::Duration(2,0);
 	ros::Rate loop_rate(2);
-	prevPos.fXPos=0;
+	prevPos.fXPos=0;   // Initializing previous position to origin and yaw angle to 45 degrees
 	prevPos.fYPos=0;
 	prevPos.fYawAngle=45.00;
 	
 	while (ros::ok()){
 		// d.sleep();
-		if(iPubFlag==1){
+		if(iPubFlag==1){  // Publish current position only when publish flag is 1
 			posPub.publish(currentPos);  // Publishes the new calculated position of i90
 		}
 		iPubFlag=0;
@@ -67,8 +67,8 @@ int main(int argc, char **argv){
 void recalculateTranslation(const std_msgs::UInt8 iTransFlag){
 	int iTransError;  // This variable stores the difference in the wheel encoder differences reading which is error in the translational motion
 	float fDistanceTravelled; // This variable stores the distance travelled by i90 from its previous position
-	iCurEnc[1]=32768-iCurEnc[1];
-	iEncoderDifference[0]=iCurEnc[0]-iPrevEnc[0];  
+	iCurEnc[1]=32768-iCurEnc[1]; // Converts the encoder values to match with left wheel.
+	iEncoderDifference[0]=iCurEnc[0]-iPrevEnc[0];   // Difference in encoder values of current and previous readings
 	iEncoderDifference[1]=iCurEnc[1]-iPrevEnc[1];
 	if(iEncoderDifference[0]<0)     // It ensures if encoder has finished loop or not. If yes then loop should be added to the difference
 		iEncoderDifference[0]=32768+iEncoderDifference[0];
@@ -87,16 +87,16 @@ void recalculateTranslation(const std_msgs::UInt8 iTransFlag){
 	currentPos.fYawAngle = prevPos.fYawAngle+(iTransError*0.235);  // Compensates error. 0.235 is angular precision of wheel encoder in degrees.
 	currentPos.fXPos = prevPos.fXPos+(fDistanceTravelled*(cos(currentPos.fYawAngle*(3.1416/180)))); // Updates position of i90
 	currentPos.fXPos = prevPos.fXPos+(fDistanceTravelled*(sin(currentPos.fYawAngle*(3.1416/180))));
-	prevPos.fYawAngle=currentPos.fYawAngle;
+	prevPos.fYawAngle=currentPos.fYawAngle;  // Updates previous position as current position to use for next calculation
 	prevPos.fXPos=currentPos.fXPos;
 	prevPos.fYPos=currentPos.fYPos;
 
-	iPubFlag=1;
+	iPubFlag=1;  //sets publish flag 
 	
 	
 
 	
-	ROS_INFO("I heard: [%f]/t[%f]", currentPos.fXPos, currentPos.fYPos);
+	ROS_INFO("New position after translational motion: [%f]/t[%f]/t", currentPos.fXPos, currentPos.fYPos,currentPos.fYawAngle);
 }
 void recalculateRotation(const std_msgs::UInt8 iRotFlag){
 	int iRotError;  // This variable stores the difference in the wheel encoder differences reading which is error in the rotational motion
@@ -123,20 +123,20 @@ void recalculateRotation(const std_msgs::UInt8 iRotFlag){
 	else
 		currentPos.fYawAngle = currentPos.fYawAngle+(iEncoderDifference[1]*0.235);
 
-	prevPos.fYawAngle=currentPos.fYawAngle;
+	prevPos.fYawAngle=currentPos.fYawAngle;   // Updates previous position as current position to use for next calculation
 	prevPos.fXPos=currentPos.fXPos;
 	prevPos.fYPos=currentPos.fYPos;
-	iPubFlag=1;
 	
 	
-ROS_INFO("I heard: [%f]", currentPos.fYawAngle);
+	
+ROS_INFO("New position after rotational motion: [%f]/t[%f]/t", currentPos.fXPos, currentPos.fYPos,currentPos.fYawAngle);
 }
 
 void updateEncoder(const drrobot_I90_player::MotorInfoArray motorInfoArray){
 	
-	iCurEnc[0] = motorInfoArray.motorInfos[0].encoder_pos;
+	iCurEnc[0] = motorInfoArray.motorInfos[0].encoder_pos;  // Reads the encoder values and store in the local variable
 	iCurEnc[1] = motorInfoArray.motorInfos[1].encoder_pos;
 	
 
-	ROS_INFO("I heard: [%d]/t[%d]", iCurEnc[0],iCurEnc[1]);
+	ROS_INFO("New encoder readings:left wheel: [%d]/tRight wheel: [%d]", iCurEnc[0],iCurEnc[1]);
 }
